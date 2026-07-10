130d5b280f2fe8ce56995366ba0aae1236bc585d
NdangaHeritier <ndangahertier@gmail.com>
2026-07-10 10:53:35 +0200

    he

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ImagePreviewProvider } from './components/UI/ImagePreviewer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ImagePreviewProvider>
+      <App />
+    </ImagePreviewProvider>
   </StrictMode>,
 )
